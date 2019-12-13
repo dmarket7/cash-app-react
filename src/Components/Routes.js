873af452be0631765f166552218aa91715f3214f
Dario Mercado <dariomercado@Darios-MacBook-Pro.local>
2019-12-13 12:23:26 -0800

    deleting some react boilerplate, adding backbone component files

---
+++ src/Components/Routes.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+
+class Routes extends Component {
+  render() {
+    return (
+      <div>Routes</div>
+    );
+  }
+}
+
+export default Routes;
